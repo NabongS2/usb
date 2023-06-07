9869181e05744ff0f9ff23880cc8cf8d533bc477
Leenahyeon <99lnh@naver.com>
2023-06-07 09:16:33 +0900

    230602_스트링 부트

---
+++ 07_SpringBoot/boardProject-boot-1/src/main/java/edu/kh/project/BoardProjectBoot1Application.java
@@ -0,0 +1,13 @@
+package edu.kh.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardProjectBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectBoot1Application.class, args);
+	}
+
+}
